922b9a413dbe632356bdfcc8676645ccdb6a7750
DelfikPro <33707322+DelfikPro@users.noreply.github.com>
2020-09-15 19:42:09 +0300

    IdentityHashMap<String, ?> -> HashMap<String, ?>

--- src/main/java/clepto/bukkit/item/Items.java
+++ src/main/java/clepto/bukkit/item/Items.java
@@ -3,24 +3,28 @@ package clepto.bukkit.item;
 import groovy.lang.Closure;
 import net.minecraft.server.v1_12_R1.ItemStack;
 
-import java.util.IdentityHashMap;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.NoSuchElementException;
 
 public class Items {
 
-	public static final Map<String, Closure<?>> items = new IdentityHashMap<>();
+	public static final Map<String, Closure<?>> items = new HashMap<>();
 
 	public static void register(String name, Closure<?> closure) {
 		items.put(name, closure);
 	}
 
 	public static ItemStack render(String address) {
-		return render(address, null);
+		return render(address, address);
 	}
 
 	public static ItemStack render(String address, Object context) {
 		ItemBuilder builder = new ItemBuilder(context);
-		builder.apply(items.get(address));
+		Closure<?> closure = items.get(address);
+		if (closure == null)
+			throw new NoSuchElementException("No item '" + address + "'");
+		builder.apply(closure);
 		return builder.build();
 	}
 
